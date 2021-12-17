ba2b3f29c6d30c81cf919eb264ae8f0cf7911e0a
mactavsin <69454747+Mactavsin@users.noreply.github.com>
2021-12-17 23:56:35 +0300

    Fixed Studio Directory Bug.

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -5,11 +5,11 @@ using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using System.Text;
 
-namespace RobloxStudioGlobalFetcher
+namespace RobloxLuauGlobalVariableFetcher
 {
     class Program
     {
-        //This attribute is required to use Clipboard functionality.
+        //This attribute is required to use Dialogs functionality.
         [STAThread]
         static void Main(string[] args)
         {
@@ -19,9 +19,9 @@ namespace RobloxStudioGlobalFetcher
             ulong IteratedMatchCount = 0;
             ulong DuplicateMatchCount = 0;
             Hashtable GlobalList = new Hashtable();
-            StringBuilder ClipboardString = new StringBuilder("");
+            StringBuilder GlobalListDataString = new StringBuilder("");
             Regex GlobalRegex = new Regex(@"[a-zA-Z_][a-zA-Z_]+", RegexOptions.Compiled);
-            string StudioExecutableDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Roblox Studio", "RobloxStudioBeta.exe");
+            string StudioExecutableDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
 
             SaveFileDialog SaveResultDialog = new SaveFileDialog();
             SaveResultDialog.Filter = "Lua Code File(*.lua)|*.lua|Luau Code File(*.luau)|*.luau";
@@ -30,9 +30,32 @@ namespace RobloxStudioGlobalFetcher
             SaveResultDialog.AddExtension = true;
             SaveResultDialog.AutoUpgradeEnabled = true;
             SaveResultDialog.RestoreDirectory = true;
-            SaveResultDialog.DefaultExt = ".lua";
+            SaveResultDialog.DefaultExt = "lua";
             SaveResultDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
             SaveResultDialog.CheckPathExists = true;
+
+            OpenFileDialog OpenStudioDirectoryDialog = new OpenFileDialog();
+            OpenStudioDirectoryDialog.Filter = "Executable File(*.exe)|*.exe";
+            OpenStudioDirectoryDialog.Title = "Select Studio Executable";
+            OpenStudioDirectoryDialog.FilterIndex = 1;
+            OpenStudioDirectoryDialog.AddExtension = false;
+            OpenStudioDirectoryDialog.AutoUpgradeEnabled = true;
+            OpenStudioDirectoryDialog.RestoreDirectory = true;
+            OpenStudioDirectoryDialog.DefaultExt = "exe";
+            OpenStudioDirectoryDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            OpenStudioDirectoryDialog.CheckPathExists = true;
+
+            //Checking if provided directy is correct or not.
+            if (OpenStudioDirectoryDialog.ShowDialog() == DialogResult.OK)
+            {
+                StudioExecutableDirectory = OpenStudioDirectoryDialog.FileName;
+            }
+            else
+            {
+                Console.WriteLine($"Failed to fetch directory {OpenStudioDirectoryDialog.FileName}, press ANY key to exit.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
 
             Console.WriteLine("Starting Operations.");
 
@@ -49,9 +72,9 @@ namespace RobloxStudioGlobalFetcher
                 //Checking if we have gone through the same global name before.
                 if (GlobalList.ContainsKey(GlobalVariable.Value) == false)
                 {
-                    ClipboardString.Append(EntryStartString);
-                    ClipboardString.Append(GlobalVariable.Value);
-                    ClipboardString.Append(EntryEndString);
+                    GlobalListDataString.Append(EntryStartString);
+                    GlobalListDataString.Append(GlobalVariable.Value);
+                    GlobalListDataString.Append(EntryEndString);
 
                     //Adding the global name to the Hashtable to prevent duplicates in later iterations.
                     GlobalList.Add(GlobalVariable.Value, true);
@@ -68,11 +91,16 @@ namespace RobloxStudioGlobalFetcher
 
             if (SaveResultDialog.ShowDialog() == DialogResult.OK)
             {
-                File.WriteAllText(SaveResultDialog.FileName, $"return {{\n{ClipboardString.ToString()}}}");
+                File.WriteAllText(SaveResultDialog.FileName, $"return {{\n{GlobalListDataString.ToString()}}}");
+                Console.WriteLine($"Data is saved to {SaveResultDialog.FileName}. Press ANY key to exit.");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to save data to {SaveResultDialog.FileName}. Press ANY key to exit.");
             }
 
-            Console.WriteLine($"Data is saved to {SaveResultDialog.FileName}. Press ANY key to exit.");
             Console.ReadLine();
+            Environment.Exit(0);
         }
     }
 }
