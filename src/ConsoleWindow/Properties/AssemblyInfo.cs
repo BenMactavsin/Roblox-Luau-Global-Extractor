356542720a92ffb8998cc92e74c8a81e3cecf4cb
mactavsin <69454747+Mactavsin@users.noreply.github.com>
2021-12-28 21:44:35 +0300

    Changes AssemblyVersion and AssemblyDescription

--- src/ConsoleWindow/Properties/AssemblyInfo.cs
+++ src/ConsoleWindow/Properties/AssemblyInfo.cs
@@ -1,18 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RobloxLuauGlobalVariableFetcher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Roblox Luau Global Variable Fetcher")]
+[assembly: AssemblyDescription("A barebone implementation of searching ASCII strings in Roblox Studio executable to create a list of possible built-in Luau globals.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RobloxLuauGlobalVariableFetcher")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("Roblox Luau Global Variable Fetcher")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-gb")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -32,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
